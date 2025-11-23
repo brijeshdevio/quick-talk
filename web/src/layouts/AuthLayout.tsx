7963badeb69b15b21e06ae254c16acfc42d60b44
Brijesh <softbrijeshio@gmail.com>
2025-11-23 13:40:42 +0530

    feat(web): add register page layout and content

--- web/src/layouts/AuthLayout.tsx
+++ web/src/layouts/AuthLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 export function AuthLayout() {
   return (
     <>
-      <main className="w-full h-screen flex items-center justify-center p-3">
+      <main className="w-full h-screen flex items-center justify-center p-3 bg-base-300">
         <Outlet />
       </main>
     </>
--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -1,3 +1,69 @@
+import { MessagesSquare } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const formFields = [
+  {
+    label: "Full Name",
+    type: "text",
+    name: "name",
+    placeholder: "Enter your full name",
+  },
+  {
+    label: "Email Address",
+    type: "email",
+    name: "email",
+    placeholder: "Enter your email address",
+  },
+  {
+    label: "Password",
+    type: "password",
+    name: "password",
+    placeholder: "***********",
+  },
+];
+
 export function Register() {
-  return <div>Register Page</div>;
+  return (
+    <section className="w-full max-w-[350px]">
+      <div>
+        <div className="p-3 rounded-2xl bg-base-100 w-fit mx-auto">
+          <MessagesSquare size={30} className="text-primary" />
+        </div>
+        <h2 className="mt-2 text-center text-2xl text-primary">QuickTalk</h2>
+      </div>
+      <div className="w-full mt-4 card bg-base-100">
+        <div className="card-body flex flex-col gap-5">
+          <div>
+            <h1 className="text-2xl text-center">Create your account</h1>
+            <p className="opacity-70 text-sm text-center">
+              Start collaborating with your team
+            </p>
+          </div>
+          <form className="flex flex-col gap-3">
+            {formFields?.map((field) => (
+              <div key={field.name}>
+                <label htmlFor={field.name}>{field.label}</label>
+                <input
+                  type={field.type}
+                  name={field.name}
+                  id={field.name}
+                  placeholder={field.placeholder}
+                  className="input input-bordered w-full"
+                />
+              </div>
+            ))}
+            <button className="btn btn-primary">Create Account</button>
+          </form>
+          <div className="text-center">
+            <p>
+              Already have an account?{" "}
+              <Link to="/login" className="text-primary">
+                Log In
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
