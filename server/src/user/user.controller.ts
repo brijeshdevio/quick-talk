2a9500767ce28bcb12e41a9cf4fad09694046d9a
Brijesh <softbrijeshio@gmail.com>
2025-11-24 11:00:01 +0530

    feat(api): add authorize user get own profile

---
+++ server/src/user/user.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import type { Response } from 'express';
+import { UserService } from './user.service';
+import { AuthGuard } from 'src/common';
+
+@UseGuards(AuthGuard)
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('profile')
+  async handleGetProfile(
+    @Req() req: { user: { sub: string } },
+    @Res() res: Response,
+  ): Promise<Response> {
+    const userId = req.user.sub;
+    const user = await this.userService.getProfile(userId);
+    return res.json({ user });
+  }
+}
---
+++ server/src/user/user.service.ts
@@ -0,0 +1,35 @@
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { isValidObjectId, Model } from 'mongoose';
+import { User } from 'src/schema/user.schema';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
+
+  private isValidMongoID(_id: string): boolean {
+    if (isValidObjectId(_id)) return true;
+    throw new BadRequestException(`Invalid ID: ${_id}`);
+  }
+
+  async getProfile(userId: string) {
+    this.isValidMongoID(userId);
+    const user = await this.userModel
+      .findById(userId)
+      .lean()
+      .select('-__v -password');
+    if (user) {
+      return user;
+    }
+
+    throw new UnauthorizedException(
+      'You are not authorized to access this resource.',
+    );
+  }
+}
